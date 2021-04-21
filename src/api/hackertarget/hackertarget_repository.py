5587535a1d4c642e080ce68c21a5b97c4cadd5b2
Peter Aleksander Bizjak <peter.aleksander.bizjak@ipmgroup.si>
2021-04-21 17:44:52 +0200

    Minor updates

--- src/api/hackertarget/hackertarget_repository.py
+++ src/api/hackertarget/hackertarget_repository.py
@@ -9,16 +9,18 @@ class HackerTargetRepository:
         self.__provider: ApiProvider = ApiProvider('api.hackertarget.com')
         self.__domain: str = domain
 
+    @property
     async def dns_lookup(self) -> str:
         return await self.__provider.get_request('dnslookup', {'q': self.__domain})
 
+    @property
     async def dns_host_records(self) -> str:
         return await self.__provider.get_request('hostsearch', {'q': self.__domain})
 
+    @property
     async def geo_ip(self) -> str:
-        result = await self.__provider.get_request(
-            'geoip',
-            {'q': gethostbyname(self.__domain)}
-        )
-        regex = r'(?<=Country: )[\w\s]+$'
-        return list(finditer(regex, result, MULTILINE))[0].group(0)
+        return list(finditer(
+            r'(?<=Country: )[\w\s]+$',
+            await self.__provider.get_request('geoip', {'q': gethostbyname(self.__domain)}),
+            MULTILINE
+        ))[0].group(0)
